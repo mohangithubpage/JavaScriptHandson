63814a4a698c8a7c02c75e696f0038af6826d8e2
mohangithubpage <167855555+mohangithubpage@users.noreply.github.com>
2025-08-17 09:04:25 +0530

    Add files via upload
    Day 01 Commit

---
+++ Day1Homework.js
@@ -0,0 +1,38 @@
+// Starting the day on hands on with let primitive data type is in java script
+
+let firstNameLet = 'Mohanaj'
+let employeeidLet = 12345
+let companyLet = "HCL"
+let isTesterLet = true
+let ageLet
+
+console.log ("========================== let primitive data type======================================")
+console.log("firstNameLet" + "  "+ firstNameLet, typeof firstNameLet)
+console.log("employeeidLet" + "  "+ employeeidLet, typeof employeeidLet)
+console.log("companyLet" + "  "+ companyLet, typeof companyLet)
+console.log("isTesterLet" + "  "+ isTesterLet, typeof isTesterLet)
+console.log("ageLet" + "  "+ ageLet, typeof ageLet)
+
+//Statrting with the var primitive data type in java script
+console.log ("========================= var primitive data type=======================================")
+var firstNameVar = "Mohanraj"
+var employeeidVar = 12345
+var companyVar = "HCL"
+var isTesterVar = true
+var ageVar = 25
+console.log("ageVar" + "  "+ ageVar, typeof ageVar)
+//var primitive data type allow to redeclare and redefine.
+var ageVar = 'newage'
+console.log("firstNameVar" + "  "+ firstNameVar, typeof firstNameVar)
+console.log("employeeidVar" + "  "+ employeeidVar, typeof employeeidVar)
+console.log("companyVar" + "  "+ companyVar, typeof companyVar)
+console.log("isTesterVar" + "  "+ isTesterVar, typeof isTesterVar)
+console.log("ageVar" + "  "+ ageVar, typeof ageVar)
+console.log ("====================== constant examples==========================================")
+//constant examples
+const center = "Testleaf"
+console.log("Name of the center"  + " " + center, typeof center)
+// center = "New Center" This will display the error message as constant value cant change it
+console.log("Name of the New center"  + " " + center, typeof center)
+console.log ("======================= Scope Testing ============================================")
+
